8ff7c690b0e58f7ff30e042d62b3141abbaa329d
tipischi <tilpietschmann@gmail.com>
2025-06-16 11:39:05 +0200

    Base Test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float playerhp = 100;
+        float enemyhp = 100;
+        float damageplayer = 0;
+        float damageenemy = 0;
+        Console.WriteLine("Damage Player:");
+        
+        damageplayer = float.Parse(Console.ReadLine());
+
+        playerhp -= damageplayer;
+        
+        Console.WriteLine(playerhp);
+    }
+}
