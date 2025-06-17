e4a94f62fa5e77c2feda084560e80e56268ca7fc
tipischi <tilpietschmann@gmail.com>
2025-06-17 10:47:10 +0200

    Base Functionality added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,113 @@
 ﻿using System;
+using System.Threading;
 
 class Program
 {
     static void Main()
     {
+        Console.WriteLine("setting up game");
         float playerHp = 100;
         float enemyHp = 100;
-        float damagePlayer = 0;
-        float damageEnemy = 0;
         float roundNumber = 0;
-        Console.WriteLine("Round " + roundNumber + ":");
+        string playerAction = "";
         
-        damagePlayer = float.Parse(Console.ReadLine());
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.White;
+        
+        while (true)
+        {
+            Console.Clear();
+            roundNumber += 1;
+            GameDrawFrame(roundNumber, playerHp, enemyHp);
+            Console.WriteLine("Enter A for ATTACK, D for DEFEND and H for HEAL:");
+            string enemyAction = "";
+            Random state = new Random();
+            int randomState = state.Next(1, 4);
+            
+            playerAction = Console.ReadLine();
 
-        playerHp -= damagePlayer;
+            Console.Clear();
+            
+            GameDrawFrame(roundNumber, playerHp, enemyHp);
+            
+            if (playerAction == "a")
+                Console.WriteLine("attacking");
+            else if (playerAction == "d")
+                Console.WriteLine("defending");
+            else if (playerAction == "h")
+                Console.WriteLine("healing");
+            
+            if (randomState == 1)
+            {
+                enemyAction = "a";
+                Console.WriteLine("Enemy is Attacking");
+            }
+            else if (randomState == 2)
+            {
+                enemyAction = "d";
+                Console.WriteLine("Enemy is Defending");
+            }
+            else if (randomState == 3)
+            {
+                enemyAction = "h";
+                Console.WriteLine("Enemy is Healing");
+            }
+
+            Thread.Sleep(2000);
+            (playerHp, enemyHp) = GameProcessActions(playerHp, enemyHp, playerAction, enemyAction);
+        }
+    }
+
+    static void GameDrawFrame(float roundNumber, float playerHp, float enemyHp)
+    {
+        Console.WriteLine("Round " + roundNumber + ":");
+        Console.Write("----------  PlayerHP = " + playerHp + "  ");
+        Console.WriteLine("EnemyHP = " + enemyHp + "  ----------");
+    }
         
-        Console.WriteLine(playerHp);
+    static (float playerHp, float enemyHp) GameProcessActions(float playerHp, float enemyHp, string playerAction, string enemyAction)
+    {
+        if (playerAction == "a")
+        {
+            if (enemyAction == "a")
+            {
+                playerHp -= 5;
+                enemyHp -= 5;
+            }
+
+            if (enemyAction == "h")
+            {
+                enemyHp -= 10;
+            }
+            
+        }
+        
+        if (playerAction == "d")
+        {
+            if (enemyAction == "h")
+            {
+                enemyHp += 10;
+            }
+        }
+        
+        if (playerAction == "h")
+        {
+            if (enemyAction == "a")
+            {
+                playerHp -= 10;
+                
+            }
+
+            if (enemyAction == "h")
+            {
+                enemyHp += 10;
+                playerHp += 10;
+            }
+            if (enemyAction == "d")
+            {
+                playerHp += 10;
+            }
+        }
+        return (playerHp, enemyHp);
     }
 }
